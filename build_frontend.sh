051ff96c929d46f2ef9f540c990356f6d1f5946d
Nathan Wallace <nwallace6988@gmail.com>
2022-11-01 17:32:20 -0600

    Add build script

---
+++ build_frontend.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+STARTDIR=`pwd`
+BASEDIR=`dirname $0`
+PROJECTDIR="$BASEDIR/build/dnt-roster"
+
+if  [ ! -e $BASEDIR/build/dnt-roster ]; then
+    echo "DNT ROSTER PROJECT NOT FOUND, CLONING..........."
+    mkdir -p $BASEDIR/build/dnt-roster
+    git clone https://github.com/nwallace83/dnt-roster.git $BASEDIR/build/dnt-roster/
+fi
+
+cd $PROJECTDIR
+git restore .
+git reset
+git clean -f
+git clean -fd
+git pull
+npm install
+npm run build
+cd $STARTDIR
+
+if [ ! -e $BASEDIR/src/html ]; then
+    mkdir $BASEDIR/src/html
+fi
+
+if [ $? -eq 0 ] 
+then
+    echo "REMOVING FILES"
+    rm -Rfv $BASEDIR/src/html/*
+    echo "COPYING FILES"
+    cp -Rfv $PROJECTDIR/build/* $BASEDIR/src/html/
+else 
+    echo "Package build failed"
+fi
