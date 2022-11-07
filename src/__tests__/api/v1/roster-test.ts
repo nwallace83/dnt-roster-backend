6cf38ba542a7c0c8036e4a9f21c0bc368e46fd79
Nathan Wallace <nwallace6988@gmail.com>
2022-11-06 22:46:22 -0700

    refactor tests

--- src/__tests__/api/v1/roster-test.ts
+++ src/__tests__/api/v1/roster-test.ts
@@ -1,26 +1,21 @@
-import app from '../../../app'
+/* eslint-disable import/first */
 import supertest from 'supertest'
 
-jest.mock('../../../services/db-character-service', () => {
-  const mockTestCharacters = [
-    { id: 'test1' },
-    { id: 'test2' },
-    { id: 'test3' }
-  ]
+const mockTestCharacters = [
+  { id: 'test1' },
+  { id: 'test2' },
+  { id: 'test3' }
+]
+let mockFindCharacter = jest.fn().mockResolvedValueOnce(mockTestCharacters).mockRejectedValueOnce('')
+jest.doMock('../../../services/db-character-service', () => {
   return {
-    findCharacters: jest.fn()
-      .mockResolvedValueOnce(mockTestCharacters)
-      .mockRejectedValueOnce('')
+    findCharacters: mockFindCharacter
   }
 })
 
-describe('roster', () => {
-  const mockTestCharacters = [
-    { id: 'test1' },
-    { id: 'test2' },
-    { id: 'test3' }
-  ]
+import app from '../../../app'
 
+describe('roster', () => {
   test('GET /roster returns roster', async () => {
     expect.assertions(1)
     return supertest(app).get('/api/v1/roster').expect(200).then(response => {
@@ -29,6 +24,7 @@ describe('roster', () => {
   })
 
   test('GET /roster returns 401 if unable to retrieve characters', async () => {
+    mockFindCharacter = jest.fn().mockRejectedValue('')
     expect.assertions(1)
     return supertest(app).get('/api/v1/roster').expect(401).then(response => {
       expect(response.text).toEqual('Unable to retrieve characters')
